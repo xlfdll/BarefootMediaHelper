5e4b2427dab0bde840ec09eb38f4b1cfd4ce2c3c
xlfdll <xlfdll@gmail.com>
2020-05-12 22:53:02 -0700

    Enabled .NET Framework BuildInfo.cs generation

--- generate-build-number.sh
+++ generate-build-number.sh
@@ -9,20 +9,20 @@ commithash=`git show -s --format=%H`
 echo "Generating build $build ($builddate $buildtime)..."
 
 # For .NET Framework projects
-# echo "// Auto-generated build information code file" > BuildInfo.cs
-# echo "// (C) Xlfdll Workstation" >> BuildInfo.cs
-# echo "" >> BuildInfo.cs
-# echo "namespace Xlfdll" >> BuildInfo.cs
-# echo "    {" >> BuildInfo.cs
-# echo "    internal static class BuildInfo" >> BuildInfo.cs
-# echo "        {" >> BuildInfo.cs
-# echo "        internal const string Version = \"$buildversion\";" >> BuildInfo.cs
-# echo "        internal const string Build = \"$build\";" >> BuildInfo.cs
-# echo "        internal const string BuildDate = \"$builddate\";" >> BuildInfo.cs
-# echo "        internal const string BuildTime = \"$buildtime\";" >> BuildInfo.cs
-# echo "        internal const string CommitHash = \"$commithash\";" >> BuildInfo.cs
-# echo "        }" >> BuildInfo.cs
-# echo "    }" >> BuildInfo.cs
+echo "// Auto-generated build information code file" > BuildInfo.cs
+echo "// (C) Xlfdll Workstation" >> BuildInfo.cs
+echo "" >> BuildInfo.cs
+echo "namespace Xlfdll" >> BuildInfo.cs
+echo "    {" >> BuildInfo.cs
+echo "    internal static class BuildInfo" >> BuildInfo.cs
+echo "        {" >> BuildInfo.cs
+echo "        internal const string Version = \"$buildversion\";" >> BuildInfo.cs
+echo "        internal const string Build = \"$build\";" >> BuildInfo.cs
+echo "        internal const string BuildDate = \"$builddate\";" >> BuildInfo.cs
+echo "        internal const string BuildTime = \"$buildtime\";" >> BuildInfo.cs
+echo "        internal const string CommitHash = \"$commithash\";" >> BuildInfo.cs
+echo "        }" >> BuildInfo.cs
+echo "    }" >> BuildInfo.cs
 
 # For .NET Core projects
 # echo "<!-- Auto-generated build information code file -->" > Directory.Build.props
