d3c0fc6c90b6e4d9ad7d02d06a61916c094b113e
xlfdll <xlfdll@gmail.com>
2020-06-30 21:31:54 -0700

    Added About screen

--- BarefootVideoHelper/ViewModels/MainViewModel.cs
+++ BarefootVideoHelper/ViewModels/MainViewModel.cs
@@ -1,4 +1,10 @@
-﻿namespace BarefootVideoHelper
+﻿using System;
+
+using Xlfdll.Diagnostics;
+using Xlfdll.Windows.Presentation;
+using Xlfdll.Windows.Presentation.Dialogs;
+
+namespace BarefootVideoHelper
 {
     public class MainViewModel : BaseViewModel
     {
@@ -6,5 +12,19 @@
             = new BBCompositionViewModel();
         public SubtitleRemovalViewModel SubtitleRemovalViewModel { get; }
             = new SubtitleRemovalViewModel();
+
+        public RelayCommand<Object> AboutCommand
+            => new RelayCommand<Object>
+            (
+                delegate
+                {
+                    AboutWindow aboutWindow = new AboutWindow
+                        (App.Current.MainWindow,
+                        AssemblyMetadata.EntryAssemblyMetadata,
+                        new ApplicationPackUri("/Images/Barefoot.png"));
+
+                    aboutWindow.ShowDialog();
+                }
+            );
     }
 }
